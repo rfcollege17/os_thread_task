df9d40056ba827f63a4b35683306a7beedd341e0
Eduardo Canellas de Oliveira <eduardocanellas@id.uff.br>
2019-10-09 13:55:27 -0300

    feita gerencia da memoria compartilhada

--- src/parte3/main.c
+++ src/parte3/main.c
@@ -11,11 +11,16 @@
 #include "../../header/parte3/teste.h"
 #include "../../lib/vet/vet.h"
 
-#define TAM 100000;
+void print_error(char *text)
+{
+  printf("\e[31m%s\e[m", text);
+}
+
+#define TAM 100000
 
 int rc; // return code
 
-// Semáforo
+/* Semáforo */
 int sem_set_id;
 /* Inicializacao das estruturas para manipular o semaforo */
 struct sembuf process_semWait = {
@@ -28,48 +33,58 @@ struct sembuf process_semSignal = {
     .sem_op = 1,
     .sem_flg = 0};
 
-// Memória compartilhada
+/* Memória compartilhada */
 int shm_id;
 void *shm_addr;
 
 int main(void)
 {
-  /* Inicializacao do semaforo no sistema. Requer iniciar
-  um conjunto de semaforos com 1 elemento e inicializar o
-  valor desse semaforo com 1 (no caso da Exclusao Mutua). */
+  /* Inicializacao do semaforo no sistema. */
 
+  /* Iniciar um conjunto de semaforos com 1 elemento */
   sem_set_id = semget(IPC_PRIVATE, 1, IPC_CREAT | IPC_EXCL | S_IRWXU);
   if (sem_set_id == -1)
   {
-    printf("deu ruim quando fui criar o sem_set\n");
+    print_error("deu ruim quando fui criar o sem_set\n");
     exit(1);
   }
 
+  /* inicializar o valor desse semaforo com 1 (por ser Exclusao Mutua). */
   rc = semctl(sem_set_id, 0, SETVAL, 1);
   if (rc == -1)
   {
-    printf("Deu ruim ao inicializar o semáforo\n");
+    print_error("Deu ruim ao inicializar o semáforo\n");
     exit(1);
   }
 
   /* A partir disso, e so usar "wait" e "signal" para
   manipular o semaforo antes e depois da zona crítica. */
+
+  /* Inicializacao da Memoria Compartilhada */
+  shm_id = shmget(IPC_PRIVATE, sizeof(int) * TAM + 5, IPC_CREAT | IPC_EXCL | S_IRWXU);
+  if (shm_id == -1)
+  {
+    print_error("deu erro na memoria compartilhada\n");
+    exit(1);
+  }
+
+  /* Obter endereco virtual relativo ao processo atual
+  a partir do id da memoria compartilhada */
+  shm_addr = shmat(shm_id, NULL, 0);
+  if (!shm_addr)
+  {
+    print_error("deu erro no endereço da memoria compartilhada\n");
+    exit(1);
+  }
+
+  /* No fim, remover a memoria compartilhada */
+  rc = shmctl(shm_id, IPC_RMID, NULL);
+  if (rc == -1)
+  {
+    print_error("deu erro ao remover a memoria compartilhada\n");
+    exit(1);
+  }
 }
-
-// Memoria compartilhada
-// shm_id = shmget(100, sizeof(int) * TAM, IPC_CREAT | IPC_EXCL | 0600);
-// if (shm_id == -1)
-// {
-//   printf("deu erro na memoria compartilhada\n");
-//   exit(1);
-// }
-
-// shm_addr = shmat(shm_id, NULL, 0);
-// if (!shm_addr)
-// {
-//   printf("deu ruim no endereço da memoria compartilhada\n");
-//   exit(1);
-// }
 
 // Birfucacao de Processos
 // pid_t child_process = fork();
